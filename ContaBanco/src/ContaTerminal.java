5312a01f1d8cb46ddb7814b9baddba9ead847db7
Igor Frutuoso <79484560+IgorFrutuoso@users.noreply.github.com>
2024-05-13 19:23:14 -0300

    Adicionado a classe ContaTerminal onde é solicitado os dados do cliente e depois impresso.

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o seu Nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Digite o Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
